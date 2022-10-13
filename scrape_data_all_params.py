9b8e6e953a2c28aedf7eb1a591dd9eec3deddf0b
JordanDespain <112026633+JordanDespain@users.noreply.github.com>
2022-10-13 13:00:20 -0500

    Update scrape_data_all_params.py

--- scrape_data_all_params.py
+++ scrape_data_all_params.py
@@ -10,6 +10,10 @@ headers = {
     "Authorization" : "Bearer %s" % key
 }
 
+
+'''
+Using API to get 1000 results and grabbing the parameters of interest
+'''
 restaurant_data = []
 
 for offset in range(0,1000,50):
@@ -54,6 +58,10 @@ for offset in range(0,1000,50):
         break
 
 
+        
+'''
+Create list of dictionaries with parameter values from data grabbed with API
+'''
 restaurants = []
 
 for i in range(len(restaurant_data)):
@@ -74,6 +82,10 @@ for i in range(len(restaurant_data)):
 
 restaurants_sorted = sorted(restaurants, key = operator.itemgetter("id"), reverse=True)
 
+
+'''
+Writing the list of dictionaries to CSV
+'''
 with open("results.csv", "w", encoding = "utf-8", newline="") as output_file:
     dict_writer = csv.DictWriter(output_file, fieldnames=["id", "name", "is closed", "rating", "review count", "categories", "address", "coordinates", "transaction types", "price level"])
     dict_writer.writeheader()
