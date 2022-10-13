94b5d213d80e5498d781bafd3525e2ce07bc83ac
sonali4794 <sonalimishra.nitt@gmail.com>
2022-10-13 10:34:23 -0500

    wrote the code to prepare geodataframe for categories mapping

--- category_map.py
+++ category_map.py
@@ -1,52 +1,28 @@
-import requests
-import numpy as np
 import pandas as pd
-from itertools import product
-from business_details_scraping import extract_info_per_business
+import numpy as np
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import os
+from PIL import Image 
+import glob
 
-# Credentials: key, url, headers
-search_url = "https://api.yelp.com/v3/businesses/search"
-business_url = "https://api.yelp.com/v3/businesses/"
-key = "kejzjnuQGK-TuKjP3VRv0lh0BYNYwRwlnz35udZ2b8W5XyrU5vC6VZoHIaq_vdfux9AihTr3SpshUvAgIBR_ziReKf9zJQ8NiEWf_9wcCjAHdiEoe6ukWpIzWrxBY3Yx"
-headers = {
-    "Authorization" : "Bearer %s" % key, 
-    "Accept": "application/json"
-}
+%matplotlib inline
 
-# Scrape:
-restaurant_ids = []
+"""further cleaning f the raw data to suit analysis for mapping purpose"""
+df = pd.read_csv('Yelp_API_Data_Cleaned.csv')
+df.columns = [c.replace(' ', '_') for c in df.columns]
+df['price_level'] = df['price_level'].replace('$', 1)
+df['price_level'] = df['price_level'].replace('$$', 2)
+df['price_level'] = df['price_level'].replace('$$$', 3)
+df.Categories_merged = [c.replace(' ', '_') for c in df.Categories_merged]
+df.Categories_merged = [c.replace('/', '_') for c in df.Categories_merged]
+df = df.loc[df["Longitude"] < -95]
 
-for offset in range(0,1000,50):
-    parameters = {
-        "location" : "Austin TX",
-        "limit" : 50,
-        "term" : "restaurant",
-        "offset" : offset
-    }
+"""read the austin city map in shapefile format to establish a base for the points to be plotted on"""
+austin_map = gpd.read_file(os.path.join(os.getcwd(), 'austin_shapefile/geo_export_1ee4a6f6-dd8c-49f2-9aa6-8630506ef412.shp'))
 
-
-    result = requests.get(search_url, headers = headers, params = parameters)
-    if result.status_code == 200:
-        for business in result.json()["businesses"]:
-            print(".")
-            id = business["id"]
-            restaurant_ids.append(id)
-    else:
-        print(result.status_code)
-        print(result.reason)
-        break
-
-
-print(restaurant_ids)
-
-for id in restaurant_ids:
-    result = requests.get(business_url + id, headers = headers)
-    if result.status_code == 200:
-        print(".")
-        business_info = extract_info_per_business(result.text)
-        print(business_info)
-        print("")
-    else:
-        print(result.status_code)
-        print(result.reason)
-        break
+"""convert the requisite dataframe into a geopandas compatible dataframe with long and lat"""
+crs = {'init': 'epsg:4326'}
+geometry = [Point(xy) for xy in zip(df["Longitude"],df["Latitude"])]
+geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
