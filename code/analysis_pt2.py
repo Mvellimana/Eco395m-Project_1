4ada9c4cc783640ee4c8fa1f24250b9f577cb816
Mezalay <Mezalay@utexas.edu>
2022-10-13 01:07:39 -0500

    replaced dollar signs with numbers for avg purposes, and grouped variables by category to find their mean

--- code/analysis_pt2.py
+++ code/analysis_pt2.py
@@ -6,13 +6,17 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from scipy.stats import zscore
 
-
+#Cleaned Data URL from Mai branch
 url = "https://raw.githubusercontent.com/Mvellimana/Eco395m-Project_1/Data-cleaning-%26-preprocessing_Mai/Yelp_API_Data_Cleaned.csv"
 
 download = requests.get(url).content
 df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 
-df_anlz = df[['categories', 
+
+#Select needed variables
+
+df_anlz = df[[
+'categories', 
 'Categories_merged', 
 'price level', 
 'review count', 
@@ -20,4 +24,21 @@ df_anlz = df[['categories',
 'rating', 
 'Rating_stndized', 
 'Zipcode']].copy()
+
+
+#Replace dollar signs to use it later for price avg/category
+
+df_anlz['price level'] = df_anlz['price level'].replace("$", 1)
+df_anlz['price level'] = df_anlz['price level'].replace("$$", 2)
+df_anlz['price level'] = df_anlz['price level'].replace("$$$", 3)
+df_anlz['price level'] = df_anlz['price level'].replace("$$$$", 4)
+
+#group variables by category to find their mean
+
+df_anlz_sum = df_anlz[[
+'Categories_merged', 
+'price level', 
+'review count', 
+'rating', 
+'Rating_stndized']].groupby(["Categories_merged"]).mean()
 
