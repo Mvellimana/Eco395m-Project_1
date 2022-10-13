b3ae1d194c15d7425702b38f7cef32b450d9b56f
Mvellimana <90660051+Mvellimana@users.noreply.github.com>
2022-10-13 14:34:03 -0500

    Update price_range_map.py

--- code/price_range_map.py
+++ code/price_range_map.py
@@ -8,20 +8,20 @@ import os
 %matplotlib inline
 
 """further cleaning f the raw data to suit analysis for mapping purpose"""
-df = pd.read_csv('Yelp_API_Data_Cleaned.csv')
-df.columns = [c.replace(' ', '_') for c in df.columns]
-df['price_level'] = df['price_level'].replace('$', 1)
-df['price_level'] = df['price_level'].replace('$$', 2)
-df['price_level'] = df['price_level'].replace('$$$', 3)
-df.Categories_merged = [c.replace(' ', '_') for c in df.Categories_merged]
-df.Categories_merged = [c.replace('/', '_') for c in df.Categories_merged]
+df = pd.read_csv("Yelp_API_Data_Cleaned.csv")
+df.columns = [c.replace(" ", "_") for c in df.columns]
+df["price_level"] = df["price_level"].replace("$", 1)
+df["price_level"] = df["price_level"].replace("$$", 2)
+df["price_level"] = df["price_level"].replace("$$$", 3)
+df.Categories_merged = [c.replace(" ", "_") for c in df.Categories_merged]
+df.Categories_merged = [c.replace("/", "_") for c in df.Categories_merged]
 df = df.loc[df["Longitude"] < -95]
 
 """read the austin city map in shapefile format to establish a base for the points to be plotted on"""
-austin_map = gpd.read_file(os.path.join(os.getcwd(), 'austin_shapefile/geo_export_1ee4a6f6-dd8c-49f2-9aa6-8630506ef412.shp'))
+austin_map = gpd.read_file(os.path.join(os.getcwd(), "austin_shapefile/geo_export_1ee4a6f6-dd8c-49f2-9aa6-8630506ef412.shp"))
 
 """convert the requisite dataframe into a geopandas compatible dataframe with long and lat"""
-crs = {'init': 'epsg:4326'}
+crs = {"init": "epsg:4326"}
 geometry = [Point(xy) for xy in zip(df["Longitude"],df["Latitude"])]
 geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 
@@ -34,6 +34,6 @@ geo_df[geo_df["price_level"] == 3].plot(ax=ax, markersize=20, color="yellow", ma
 ax.set_title("Restaurants in Austin City based on Prices") 
 plt.legend(prop={"size": 15})
 
-fig.savefig('gif/price_range_map.png', 
-              dpi=100, format='png', 
-              bbox_inches='tight')
+fig.savefig("gif/price_range_map.png", 
+              dpi=100, format="png", 
+              bbox_inches="tight")
