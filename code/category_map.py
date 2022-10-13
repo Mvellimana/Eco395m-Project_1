46a9efa6ebac70a4315815583a9d5b911284b7b1
sonali4794 <sonalimishra.nitt@gmail.com>
2022-10-13 17:24:43 -0500

    updated the path

--- code/category_map.py
+++ code/category_map.py
@@ -7,10 +7,9 @@ import os
 from PIL import Image 
 import glob
 
-%matplotlib inline
 
-"""further cleaning f the raw data to suit analysis for mapping purpose"""
-df = pd.read_csv("Yelp_API_Data_Cleaned.csv")
+#further cleaning f the raw data to suit analysis for mapping purpose
+df = pd.read_csv("artifacts/Yelp_API_Data_Cleaned.csv")
 df.columns = [c.replace(" ", "_") for c in df.columns]
 df["price_level"] = df["price_level"].replace("$", 1)
 df["price_level"] = df["price_level"].replace("$$", 2)
@@ -19,15 +18,15 @@ df.Categories_merged = [c.replace(" ", "_") for c in df.Categories_merged]
 df.Categories_merged = [c.replace("/", "_") for c in df.Categories_merged]
 df = df.loc[df["Longitude"] < -95]
 
-"""read the austin city map in shapefile format to establish a base for the points to be plotted on"""
-austin_map = gpd.read_file(os.path.join(os.getcwd(), "austin_shapefile/geo_export_1ee4a6f6-dd8c-49f2-9aa6-8630506ef412.shp"))
+#read the austin city map in shapefile format to establish a base for the points to be plotted on
+austin_map = gpd.read_file(os.path.join(os.getcwd(), "data/austin_shapefile/geo_export_1ee4a6f6-dd8c-49f2-9aa6-8630506ef412.shp"))
 
-"""convert the requisite dataframe into a geopandas compatible dataframe with long and lat"""
+#convert the requisite dataframe into a geopandas compatible dataframe with long and lat
 crs = {"init": "epsg:4326"}
 geometry = [Point(xy) for xy in zip(df["Longitude"],df["Latitude"])]
 geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 
-"""plot a graph fr every food category and save them using a loop"""
+#plot a graph fr every food category and save them using a loop
 list_of_categories = ["American",
                       "Mexican_South_American",
                       "Bars_Pubs",
@@ -46,22 +45,22 @@ def plot_foodcategory(category):
     ax.set_title("Food Category = " + str(category))
     return ax,fig
 
-
+#run a for loop to save plots for individual categories
 for category in list_of_categories:
     ax, fig = plot_foodcategory(category)
-    fig.savefig("gif/{}.png".format(category), 
+    fig.savefig("artifacts/figures/{}.png".format(category), 
               dpi=100, format="png", 
               bbox_inches="tight")
     plt.close()
 
-"""write a code to merge the individual graph images into a gif"""
+#write a code to merge the individual graph images into a gif
 frames = []
-imgs = glob.glob("gif/*.png")
+imgs = glob.glob("artifacts/figures/*.png")
 for i in imgs:
     new_frame = Image.open(i)
     frames.append(new_frame)
  
-frames[0].save("gif/category.gif", format="GIF",
+frames[0].save("artifacts/figures/category.gif", format="GIF",
                append_images=frames[1:],
                save_all=True,
                duration=1000, loop=0)
--- code/price_range_map.py
+++ code/price_range_map.py
@@ -5,10 +5,9 @@ import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import os
 
-%matplotlib inline
 
-"""further cleaning f the raw data to suit analysis for mapping purpose"""
-df = pd.read_csv("Yelp_API_Data_Cleaned.csv")
+#further cleaning f the raw data to suit analysis for mapping purpose
+df = pd.read_csv("artifacts/Yelp_API_Data_Cleaned.csv")
 df.columns = [c.replace(" ", "_") for c in df.columns]
 df["price_level"] = df["price_level"].replace("$", 1)
 df["price_level"] = df["price_level"].replace("$$", 2)
@@ -17,15 +16,15 @@ df.Categories_merged = [c.replace(" ", "_") for c in df.Categories_merged]
 df.Categories_merged = [c.replace("/", "_") for c in df.Categories_merged]
 df = df.loc[df["Longitude"] < -95]
 
-"""read the austin city map in shapefile format to establish a base for the points to be plotted on"""
-austin_map = gpd.read_file(os.path.join(os.getcwd(), "austin_shapefile/geo_export_1ee4a6f6-dd8c-49f2-9aa6-8630506ef412.shp"))
+#read the austin city map in shapefile format to establish a base for the points to be plotted on
+austin_map = gpd.read_file(os.path.join(os.getcwd(), "data/austin_shapefile/geo_export_1ee4a6f6-dd8c-49f2-9aa6-8630506ef412.shp"))
 
-"""convert the requisite dataframe into a geopandas compatible dataframe with long and lat"""
+#convert the requisite dataframe into a geopandas compatible dataframe with long and lat
 crs = {"init": "epsg:4326"}
 geometry = [Point(xy) for xy in zip(df["Longitude"],df["Latitude"])]
 geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 
-"""plot a graph showing all locations of restaurants in the dataset and highlight different price levels"""
+#plot a graph showing all locations of restaurants in the dataset and highlight different price levels
 fig,ax = plt.subplots(figsize = (15,15))
 austin_map.plot(ax=ax, alpha=0.4, color="xkcd:sky blue")
 geo_df[geo_df["price_level"] == 1].plot(ax=ax, markersize=20, color="red", marker="o", label="Low Price Level")
@@ -34,6 +33,6 @@ geo_df[geo_df["price_level"] == 3].plot(ax=ax, markersize=20, color="yellow", ma
 ax.set_title("Restaurants in Austin City based on Prices") 
 plt.legend(prop={"size": 15})
 
-fig.savefig("gif/price_range_map.png", 
+fig.savefig("artifacts/figures/price_range_map.png", 
               dpi=100, format="png", 
               bbox_inches="tight")
